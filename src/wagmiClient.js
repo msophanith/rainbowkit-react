6fd4e72b3115ff4a9fa9fd6d5e3c6808d5594c89
mey.sophanith <mey.sophanith@sabay.com>
2023-03-03 15:10:26 +0700

    env: updated env

--- src/wagmiClient.js
+++ src/wagmiClient.js
@@ -6,10 +6,7 @@ import { publicProvider } from "wagmi/providers/public";
 
 export const { chains, provider } = configureChains(
   [mainnet, polygon, optimism, arbitrum],
-  [
-    alchemyProvider({ apiKey: "ET44S002jilOmxF-XR7KeMg7Ih9sYynr" }),
-    publicProvider(),
-  ]
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_API }), publicProvider()]
 );
 const { connectors } = getDefaultWallets({
   appName: "My RainbowKit App",
